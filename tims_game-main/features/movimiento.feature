96a4a90d6f110722929cced0ba881c7bf9c07d6e
Aldair-Belisario <aldair.belisario@estudiante.ucsm.edu.pe>
2025-06-25 13:41:03 -0500

    Delete tims_game-main directory

--- tims_game-main/features/movimiento.feature
+++
@@ -1,6 +0,0 @@
-Feature: Movimiento básico del jugador
-
-  Scenario: El jugador se mueve hacia la derecha
-    Given el jugador está en x = 0
-    When se presiona solo la tecla derecha
-    Then la posición x del jugador debe aumentar
--- tims_game-main/real_tims/constants.py
+++
@@ -1,8 +0,0 @@
-# constants.py
-WIDTH = 800      # Ancho de la pantalla (también límite derecho del jugador)
-HEIGHT = 600     # Alto de la pantalla
-FPS = 60         # Fotogramas por segundo
-
-WHITE = (255, 255, 255)  # Color de fondo
-BLUE = (0, 100, 255)     # Color del jugador
-BLACK = (0, 0, 0)        # Color para texto u otros elementos
